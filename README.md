480487e109ae8d7195e8a263ae909ccf9b8a816c
NAKAJIMA Yusaku <yupong7@gmail.com>
2022-05-19 12:42:18 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Gimei - It's new $module
+
+# SYNOPSIS
+
+    use App::Gimei;
+
+# DESCRIPTION
+
+App::Gimei is ...
+
+# LICENSE
+
+Copyright (C) NAKAJIMA Yusaku.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+NAKAJIMA Yusaku <yupong7@gmail.com>
---
+++ lib/App/Gimei.pm
@@ -0,0 +1,39 @@
+package App::Gimei;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Gimei - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Gimei;
+
+=head1 DESCRIPTION
+
+App::Gimei is ...
+
+=head1 LICENSE
+
+Copyright (C) NAKAJIMA Yusaku.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+NAKAJIMA Yusaku E<lt>yupong7@gmail.comE<gt>
+
+=cut
+
